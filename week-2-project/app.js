7956454275e1272573a861af7041a2e21538c1da
elif <elifsancak252@gmail.com>
2019-11-08 22:40:56 +0100

    again edited

--- week-2-project/app.js
+++ week-2-project/app.js
@@ -17,43 +17,19 @@ const object = {
   
   hasKey: function (obj, key) {
     // write me!
-    //obj.hasKey("a") ;
-   // console.log(hasKey({a: "3"}, "a"));
-  // return object ? hasOwnProperty.call(object, key) : false;
-  //const obj = {a:3};
-  //obj.a = 3;
-  var obj = {
-    a: 3,
-    x:4
-};
-   if (obj.hasOwnProperty("a")){
-  return (obj.hasOwnProperty({ a: 3 }, "a"));
- }
- 
-  if (obj.hasOwnProperty("x")){
-  return (obj.hasOwnProperty({ x: 4 }, "y"));
+
+  if (obj.hasOwnProperty(key)){
+  return true;
   }
-   //const obj = { a, b, x, y}
-   //a = obj;
-   //a.b = 3;
-   //console.log ('a' in obj);
-   //console.log ('x' in obj);
+  else 
+  return false;
 
   // if (a.hasOwnProperty('a')){
    //  return true;
    //}
    //else if (a.hasOwnProperty('b'))
    //return false;
-   //if (a.hasOwnProperty('a')){
-   //return true;
-   //}
-   //else if(a.hasOwnProperty('b'));
-   //return false;
-
-  //},
-  //console.log(hasKey({a: 3}, "a"));
-   
-   
+  
    //return obj === 3 && hasOwnProperty.call(obj, key);
    //return obj === 3 && hasOwnProperty.call(obj, key);
     
@@ -61,22 +37,23 @@ const object = {
   
   hasValue: function (obj, value) {
     // write me!
+    return (Object.values(obj).indexOf(value) > -1);
   },
   addEntry: function (key, value) {
-    if (null) { // write me!
+    if (typeof key!=='string') { // write me!
       return new TypeError('addEntry: key should be a string');
     }
-    if (null) { // write me! (using this.isPrimitive)
+    if (Object.value !== value) { // write me! (using this.isPrimitive)
       return new TypeError('addEntry: value should be a primitive');
     }
-    if (null) { // write me! (using this.hasKey)
+    if (obj.hasOwnProperty(key) !== this.entries) { // write me! (using this.hasKey)
       return { [key]: new Error(`addEntry: key "${key}" already exists`) };
     }
 
     // write me!
   },
   removeEntry: function (key) {
-    if (null) { // write me!
+    if (typeof key !== 'string') { // write me!
       return new TypeError('removeEntry: key should be a string');
     }
     if (null) { // write me! (using this.hasKey)
@@ -86,10 +63,10 @@ const object = {
     // write me!
   },
   updateEntry: function (key, value) {
-    if (null) { // write me!
+    if (typeof key !== 'string') { // write me!
       return new TypeError('updateEntry: key should be a string');
     }
-    if (null) { // write me! (using this.isPrimitive)
+    if (Object.value !== value) { // write me! (using this.isPrimitive)
       return new TypeError('updateEntry: value should be a primitive');
     }
     if (null) { // write me! (using this.hasKey)
@@ -102,7 +79,7 @@ const object = {
     // write me!
   },
   findByKey: function (key) {
-    if (null) { // write me!
+    if (typeof key !== 'string') { // write me!
       return new TypeError('findByKey: key should be a string');
     }
     if (null) { // write me! (using this.hasKey)
@@ -112,7 +89,7 @@ const object = {
     // write me!
   },
   findByValue: function (value) {
-    if (null) { // write me! (using this.isPrimitive)
+    if (Object.value !== value) { // write me! (using this.isPrimitive)
       return new TypeError('findByValue: value should be a primitive');
     }
     if (null) { // write me! (using this.hasValue)
